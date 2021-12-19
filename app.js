0d431ee47884f22c7dd885b63b34f04c1333c54b
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-19 15:00:03 +0530

    Day-1, part-1

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log(require('./index'))
+//"require" is a built-in function in Node JS which gives you access to the exports object of the file that provided the path of.
+//It execute the file whichever we specify.
+
+//console.log(module)
