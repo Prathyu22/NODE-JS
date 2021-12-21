fb9fd96706dc7f190290720e80ee6a203b976fb9
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-22 00:11:32 +0530

    Day-2, part-2_Paths

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
+const path = require('path')
+//console.log(path)
 
-console.log(button,input,div) // To check whether these are loaded or not.
+path.resolve('./utils.js') //relative path
+console.log(path.resolve('./utils.js')) // Absolute path --- starts from the core of the machine.
+//Resolve() --- converts the relative to the absolute path.
 
-const arr = [1,2,3,4,5]
-localStorage.setItem('inputValue',JSON.stringify(arr))
-console.log(JSON.parse(localStorage.getItem('inputValue')))
-// div.innerHTML = localStorage.getItem("inputValue")
-// button.addEventListener('click', (req,res) => {
-//     localStorage.setItem('inputValue',input.value)
-//     div.innerHTML = input.value
-// })
-//inner.HTML replaces the string.
+console.log(path.relative('/',"./utils.js"))// returns the path of temp/utils.js relative to the temp folder.
+//path.relative() --- converts into relative.
+//Right side path is relative to the left side path.
+//2 parameters ---> 1.Relative to which you want the path to do.
+//                  2. actual/absolute path or relative path of the file or folder.
+ 
+console.log(path.extname('./utils.js')) // gives you the extension nme.
 
-//localStorage.setItem('inputValue',"Something")
-//console.log(localStorage.getItem("inputValue"))
+console.log(path.dirname('./temp/utils.js')) // directory name.
+
+console.log(__dirname) // path of the current directory.
+console.log(path.join(__dirname, 'utils.js')) // same as resolve().
