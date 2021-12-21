61f0d98b516276e5b92ac25954c0359151b13464
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-21 07:37:26 +0530

    Day-2, part-1

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const {add,subtract,multiply,divide,message} = require('./utils')
-//require gives access to the export's object.
-add()
-subtract()
-multiply()
-divide()
-console.log(message)
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
+
+console.log(button,input,div) // To check whether these are loaded or not.
+
+const arr = [1,2,3,4,5]
+localStorage.setItem('inputValue',JSON.stringify(arr))
+console.log(JSON.parse(localStorage.getItem('inputValue')))
+// div.innerHTML = localStorage.getItem("inputValue")
+// button.addEventListener('click', (req,res) => {
+//     localStorage.setItem('inputValue',input.value)
+//     div.innerHTML = input.value
+// })
+//inner.HTML replaces the string.
+
+//localStorage.setItem('inputValue',"Something")
+//console.log(localStorage.getItem("inputValue"))
