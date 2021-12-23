14b3a50974cdd4b03119407b9b2c0ef22b696203
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-23 20:34:02 +0530

    Day-2 part-3_writeFile()

--- app.js
+++ app.js
@@ -2,26 +2,13 @@ const fs = require('fs') // fs --> FileSystem.
 //console.log(fs)
 //fs module ---> It allow to access your disk. Used to write, read, update delete on hard disk.
 
-fs.readFile('./text.txt', (error,data) => {
+fs.writeFile('./text.txt',"New Stuff inside the file", (error,data) => {
     if(error) 
         console.log(error)
-    else {
-        console.log(typeof data) //object
-        console.log(data) //buffer
-        console.log(data.toString()) //text given in text.txt
-    }
 })
 
-/*fs.readFile('./text.txt','utf8', (error,data) => {
+
+fs.writeFile('./text.txt',`Append me ${fs.readFileSync('./text.txt','utf8')}`, (error) => {
     if(error) 
         console.log(error)
-    else {
-        console.log(typeof data) //string
-        console.log(data) //text given in text.txt
-        console.log(data.toString()) //text given in text.txt 
-    }
-})*/
-
-console.log(fs.readFileSync('./text.txt','utf8'))
-console.log(`I'm a random log`)
- 
+})
