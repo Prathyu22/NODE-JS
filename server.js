468df53195b92ff67b9c6a186e77c1dc5f3e07d1
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-25 15:00:17 +0530

    Day-4 part-1_Public,Private routes & middleware()

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-const data = [{name: "Anisha"}, {name: "Suman"}, {name: "Varun"}]
 const express = require('express')
-const app = express() //instatiating express.
+const app = express() //instance of express. 
+const PORT = 3000
 
-app.get('/getNames', (req,res) => {
-    res.send(data) //response ---> in browser
+//middleware function
+/*const verify = (req,res,next) => {
+    if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io") next()
+    //next proceeds to the next callback() / next middleware.
+    else res.send("BLOCKED")
+}*/ 
+
+const isAdmin = (req,res,next) => {
+    if(req.headers.admin === 'true') next()
+    else res.send("UNAUTHOROISED")
+}
+
+app.get('/public', (req,res) => 
+{
+    console.log(req.headers)
+    res.send("I'm a public route")   
 })
 
-app.listen(3000, () => {
-    console.log("Server listening at PORT 3000")
+app.get('/private',isAdmin,(req,res) => {
+    res.send(`I'm an admin route`)
+}) // this cannot be runned by browser as it is a post request.
+
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`)
 })
+
+module.exports = () => {} 
