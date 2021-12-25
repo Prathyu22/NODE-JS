0889bf5ee927ce7dd4cfb6f4e067931b37bb5aa8
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-25 15:36:31 +0530

    Day-4 part-2_express.json()

--- server.js
+++ server.js
@@ -2,27 +2,12 @@ const express = require('express')
 const app = express() //instance of express. 
 const PORT = 3000
 
-//middleware function
-/*const verify = (req,res,next) => {
-    if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io") next()
-    //next proceeds to the next callback() / next middleware.
-    else res.send("BLOCKED")
-}*/ 
+app.use(express.json()) //allows us to use it as a middleware.
 
-const isAdmin = (req,res,next) => {
-    if(req.headers.admin === 'true') next()
-    else res.send("UNAUTHOROISED")
-}
-
-app.get('/public', (req,res) => 
-{
-    console.log(req.headers)
-    res.send("I'm a public route")   
+app.post('/signup',(req,res) => {
+    console.log(req.body) //Before app.use(), the body is stringified but now it is parsed to json.
+    res.send('dadada')
 })
-
-app.get('/private',isAdmin,(req,res) => {
-    res.send(`I'm an admin route`)
-}) // this cannot be runned by browser as it is a post request.
 
 app.listen(PORT, () => {
     console.log(`Server running at PORT ${PORT}`)
