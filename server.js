312b99d035e3a76065165bd87c3b3eafe38e58ea
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-26 23:11:47 +0530

    Day-6 part-1_Dynamic Route

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express() //instance of express. 
-const PORT = 3000
+const PORT = 3001
 
-app.use(express.json()) //allows us to use it as a middleware.
+app.get('/products/cases')
+app.get('/products/protection')
+app.get('/products/bands')
 
-app.post('/signup',(req,res) => {
-    console.log(req.body) //Before app.use(), the body is stringified but now it is parsed to json.
-    res.send('dadada')
+//Dynamic Route
+app.get('/products/:categories',(req,res) => {
+    console.log(req.params) //params -- parameters.
+    res.send('PING')
 })
 
 app.listen(PORT, () => {
