ca7b3229cc631d4f253f14d44066f0e07215a2f4
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-26 12:34:44 +0530

    Day-5 part-2_get,add,auth,validate

--- server.js
+++ server.js
@@ -2,15 +2,53 @@ const express = require('express')
 const app = express() //instance of express. 
 const PORT = 3000
 
-const logger = (req,res,next) => {
-    console.log(req.method)
-    next()
+app.use(express.json()) //allows us to use it as a middleware.
+
+const token = "TOP_SECRET"
+
+let products = [{name: 'iPhone12 case', price: '999'},{name: 'iPhone13 case', price: '1199'},{name: 'iPhone13 Pro case', price: '1499'} ]
+
+/* <------------------------------ AUTHORISATION MIDDLEWARE ------------------------------> */
+const isAuthorised = (req,res,next) => {
+    if(req.headers.authorisation === token) next()
+    else res.json({ error: "UNAUTHORISED"})
 }
 
-app.use(logger) //allows us to use it as a middleware.
+/* <------------------------------ VALIDATION MIDDLEWARE ------------------------------> */
+const validator = (req,res,next) => {
+    const{name,price} = req.body
 
-app.post('/',(req,res) => {
-    res.send('Server Running')
+    if(!name || !price) res.json({ error: "Validation Failed"})
+    else next()
+}
+/* <------------------------------ PUBLIC ROUTE ------------------------------> */
+//Get Route //Send all products.
+app.get('/products', (req,res) => {
+   // res.send(products) //text response ---> {...}
+    res.json({products}) //array response  ---> [...]
+})
+
+/* <------------------------------ PRIVATE ROUTE ------------------------------> */
+app.post('/products/add',isAuthorised,validator,(req,res) => {
+    const{name,price} = req.body
+    
+    products.push({
+            name,
+            price
+    })
+
+    res.send({products})
+})
+
+/* <------------------------------ AUTH ROUTE ------------------------------> */
+app.post('/auth',(req,res) => {
+    const {email,password} = req.body
+    if(email === 'admin@mail.com' && password ==='password') {
+        res.send({token})
+    } 
+    else {
+        res.send({message: "UNAUTHORISED"})
+    }
 })
 
 app.listen(PORT, () => {
