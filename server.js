807140674ad914f75f86afc0c8f938c59957ff89
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2021-12-26 09:02:12 +0530

    Day-5 part-1_app.use()-middleware

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express() //instance of express. 
 const PORT = 3000
 
-app.use(express.json()) //allows us to use it as a middleware.
+const logger = (req,res,next) => {
+    console.log(req.method)
+    next()
+}
 
-app.post('/signup',(req,res) => {
-    console.log(req.body) //Before app.use(), the body is stringified but now it is parsed to json.
-    res.send('dadada')
+app.use(logger) //allows us to use it as a middleware.
+
+app.post('/',(req,res) => {
+    res.send('Server Running')
 })
 
 app.listen(PORT, () => {
