ad686fbcb7a125d0f0bcc4165f1f757c8ef2cc6e
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-25 20:08:07 +0530

    modules, require() 2

--- people.js
+++ people.js
@@ -1,5 +1,8 @@
 const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
-
+const ages = [20, 25, 30, 35];
 //console.log(people); // [ 'yoshi', 'ryu', 'chun-li', 'mario' ]
 
-module.exports = 'hello'; 
+module.exports = {
+    //people : people => can be written as people
+    people, ages
+}; 
