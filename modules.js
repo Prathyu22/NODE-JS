a9b50930a23a7417019d834286ce765003258d92
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-26 09:34:44 +0530

    modules, require() / info of os

--- modules.js
+++ modules.js
@@ -1,4 +1,4 @@
 //require is used to import the file.
-const { people, ages } = require('./people');
+const os = require('os'); // It gives the information of our OS.
 
-console.log(people, ages);  
+console.log(os.platform(), os.homedir());
