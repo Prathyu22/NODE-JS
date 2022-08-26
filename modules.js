0866c6d7a37b5c3089027bb94a4e8ab482f97d77
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-26 21:08:11 +0530

    creating a server

--- modules.js
+++
@@ -1,4 +0,0 @@
-//require is used to import the file.
-const os = require('os'); // It gives the information of our OS.
-
-console.log(os.platform(), os.homedir());
--- people.js
+++
@@ -1,8 +0,0 @@
-const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
-const ages = [20, 25, 30, 35];
-//console.log(people); // [ 'yoshi', 'ryu', 'chun-li', 'mario' ]
-
-module.exports = {
-    //people : people => can be written as people
-    people, ages
-}; 
