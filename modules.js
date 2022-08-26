5d1bdccd5c6a9a6b333f51e373627e5b5582218f
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-26 09:30:34 +0530

    modules, require() 3

--- modules.js
+++ modules.js
@@ -1,15 +1,4 @@
 //require is used to import the file.
-const xyz = require('./people');
+const { people, ages } = require('./people');
 
-console.log(xyz); 
-/* 
-o/p = {
-    people: [ 'yoshi', 'ryu', 'chun-li', 'mario' ],
-    ages: [ 20, 25, 30, 35 ]
- }
-*/
-
-console.log(xyz.people, xyz.ages); 
-/*
-o/p = [ 'yoshi', 'ryu', 'chun-li', 'mario' ] [ 20, 25, 30, 35 ]
-*/
+console.log(people, ages);  
