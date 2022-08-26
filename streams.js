d6ccc22fe2156311b8115930886ffb3441b17664
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-26 19:52:45 +0530

    stream & buffers - piping concept

--- streams.js
+++ streams.js
@@ -4,9 +4,12 @@ const readStream = fs.createReadStream('./docs/blog1.txt', {encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog2.txt');
 
 // 'on' is an event listener. 
-readStream.on('data', (chunk) => {
+/*readStream.on('data', (chunk) => {
     console.log('----- NEW CHUNK -----');
     console.log(chunk);
     writeStream.write('\n NEW CHUNK \n');
     writeStream.write(chunk);
-});
+}); */
+
+//piping
+readStream.pipe(writeStream);
