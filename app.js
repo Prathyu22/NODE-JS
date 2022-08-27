8f5f86fea5ef7c1ba0e82d275d878bdfb7ab9bb6
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-27 11:33:40 +0530

    routing & html pages

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p> Home Page </p>');
+    res.sendFile('./views/index.html', {root: __dirname});
+    //res.send('<p> Home Page </p>');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname});
+    //res.send('<p> About Page </p>');
 });
