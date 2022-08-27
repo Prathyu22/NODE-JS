4ba2566e3de64af079af8eb48aba8d974e2241ee
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2022-08-27 09:39:38 +0530

    response object

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const http = require('http');
 const server = http.createServer( (req, res) => {
     //console.log(req);
     console.log(req.url, req.method);
+
+    //set header content type.
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello, Ninjas');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
